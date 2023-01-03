8c72626915e48277bd87c8524478ab40076a0560
Andrew Luchuk <andrew.luchuk@outlook.com>
2023-01-02 22:29:07 -0500

    adds basic config parser

--- indexor_core/config.py
+++ indexor_core/config.py
@@ -1,4 +1,24 @@
+import os
+import configparser
+
+
 class Config:
 
     def __init__(self, engine_url=""):
         self.engine_url = engine_url
+
+
+def ini_file_loader(path):
+    if not os.path.exists(path):
+        return Config()
+
+    config = configparser.ConfigParser()
+
+    config.read(path)
+
+    config.sections()
+
+    if 'server' not in config:
+        return Config()
+
+    return Config(engine_url=config['server']['engine_url'])
