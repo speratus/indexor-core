85ba5e82bd9a49661fa5f1279811e257701e5aab
Andrew Luchuk <andrew.luchuk@outlook.com>
2023-01-28 15:35:37 -0500

    adds async search function

--- indexor_core/searcher.py
+++ indexor_core/searcher.py
@@ -1,4 +1,5 @@
 import requests
+import aiohttp
 
 from indexor_core.query import Query
 
@@ -17,3 +18,19 @@ def search(terms, config):
     query = Query.build_query(terms)
 
     return run_search(query, config)
+
+
+async def run_search_async(querystring, config):
+    url = config.engine_url
+
+    full_path = f"{url}/search{querystring}"
+
+    async with aiohttp.ClientSession as session:
+        async with session.get(full_path) as response:
+            return await response.json()
+
+
+async def search_async(terms, config):
+    query = Query.build_query(terms)
+
+    return await run_search_async(query, config)
